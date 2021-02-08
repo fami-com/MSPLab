799664d5f4775989e58e45d60eb23de0fadfe280
fami-com <me@succ.dk>
2021-02-08 18:11:57 +0200

    Lab 1.2

---
+++ LibTester/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MSPLib;
+
+namespace LibTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            MSPLib.Lab.Part1();
+            Console.ReadKey();
+
+            MSPLib.Lab.Part2();
+            Console.ReadKey();
+        }
+    }
+}
